03b03889553506a4b4af5e74c47eef52c23120de
NGUYENQUANGHUY <huynq250801@tvu-onschool.edu.vn>
2025-11-14 12:35:22 +0700

    tạo giao diện trang chủ thành công

--- src/WebAppleStore/WebAppleStore/wwwroot/js/storeapple.js
+++ src/WebAppleStore/WebAppleStore/wwwroot/js/storeapple.js
@@ -2,3 +2,47 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const dotsContainer = document.querySelector(".slider-dots");
+
+// Tạo Dots tự động theo số slide
+slides.forEach((_, i) => {
+    const dot = document.createElement("span");
+    if (i === 0) dot.classList.add("active-dot");
+    dot.addEventListener("click", () => goToSlide(i));
+    dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".slider-dots span");
+
+function showSlide(index) {
+    slides.forEach(s => s.classList.remove("active"));
+    dots.forEach(d => d.classList.remove("active-dot"));
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active-dot");
+
+    currentIndex = index;
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+// Nút bấm
+document.querySelector(".next").addEventListener("click", nextSlide);
+document.querySelector(".prev").addEventListener("click", prevSlide);
+
+// Auto chạy 4s/lần
+setInterval(nextSlide, 4000);
+
+function goToSlide(index) {
+    showSlide(index);
+}
